ff6d789975737634f614431e90d6af2b58dcaa16
Zayddwikat <114936305+Zayddwikat@users.noreply.github.com>
2024-11-11 13:22:34 +0200

    add inputs

---
+++ booking-platform/src/stories/userName.jsx
@@ -0,0 +1,7 @@
+export default function UserNameField() {
+  return (
+    <label>
+      <input type="text" placeholder="User Name" /> User Name
+    </label>
+  );
+}
---
+++ booking-platform/src/stories/userName.stories.jsx
@@ -0,0 +1,12 @@
+import UserNameField from "./userName";
+
+export default {
+  title: "UserNameField",
+  component: UserNameField,
+};
+export const Default = {
+  title: "UserNameField",
+  args: {
+    label: "Username",
+  },
+};
