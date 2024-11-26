eb3496fe96a72bc97c02f3da208a72a4c24bfd0e
Zayddwikat <114936305+Zayddwikat@users.noreply.github.com>
2024-11-26 16:09:02 +0200

    Trending hotels and Trending Post's
    Add trending Hotels and Posts to the main page

---
+++ booking-platform/src/user/HomePage/component/TrendingPost.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+
+export default function TrendingPost({ post }) {
+  const handleClickedDeals = (post) => {
+    console.log("clicked", post.cityName);
+  };
+  return (
+    <div
+      onClick={() => handleClickedDeals(post)}
+      className="rounded-md w-[100dvw] lg:w-[18dvw] md:w-[70dvw]   "
+    >
+      <div className="h-[40dvh] flex flex-col items-start">
+        <img
+          className="rounded-md md:w-full w-[95dvw]"
+          loading="lazy"
+          style={{
+            height: "75%",
+            objectFit: "cover",
+          }}
+          src={post.thumbnailUrl}
+        />
+        <article className="flex flex-col  items-start md:w-full w-[100dvw] my-1">
+          <span className="flex flex-row">
+            <p className="text-sm">{post.cityName + ","} </p>
+            <p className="text-sm text-bold">{post.countryName}</p>
+          </span>
+
+          <div className=" w-full">
+            <p className="text-sm  line-clamp-2 ">{post.description}</p>
+          </div>
+        </article>
+      </div>
+    </div>
+  );
+}
+
+TrendingPost.propTypes = {
+  post: PropTypes.object.isRequired,
+};
